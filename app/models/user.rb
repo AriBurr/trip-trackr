0a0c5256442110776209d4cb106af8d7cb1bb3b8
Ariel Burriss <ariburr@gmail.com>
2017-12-02 11:28:58 -0700

    style trip index

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :trips, dependent: :destroy
+
 end
