c469f81ba6cf2653b14e8269ebc1092ac70f5f89
Austin Phillips <phillips.austin51@gmail.com>
2017-12-02 19:27:59 -0700

    Add to location views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,6 @@
 class LocationsController < ApplicationController
+before_action :set_location, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -6,8 +8,29 @@ class LocationsController < ApplicationController
   end
 
   def new
+    @location = Location.new
   end
+
+  def create
+  end 
 
   def edit
   end
+
+  def update
+  end 
+
+  def destroy
+  end 
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end 
+
+  def location_params
+    params.require(:location).permit(:name)
+  end 
+
 end
