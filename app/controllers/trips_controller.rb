d1d14437f89d3ac45d5779b38956c68ab571001f
Ariel Burriss <ariburr@gmail.com>
2017-12-03 13:55:39 -0700

    fill out trip show view and add desc to form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -45,7 +45,7 @@ class TripsController < ApplicationController
   end
 
   def trip_params
-    params.require(:trip).permit(:name, :date_start, :date_end, :image)
+    params.require(:trip).permit(:name, :date_start, :date_end, :image, :desc)
   end
 
 end
