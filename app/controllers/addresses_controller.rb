946e1c9b1add7c079efd2f5c88e63a39a3ac6ef1
Aaron_Winn <winn403@hotmail.com>
2017-12-03 14:06:18 -0700

    address c

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,42 @@
 class AddressesController < ApplicationController
   def index
+    
   end
 
   def show
+
   end
 
+  def create
+    @address = current_user.address.create(address_params)
+    if @address.save
+      redirect_to address_path
+    else
+      render :new
+    end
+  end 
+
   def new
+    @address = Address.new
   end
 
   def edit
+    
+
   end
+
+  def destroy
+
+  end 
+
+
+  private
+  def set_address
+    @address = current_user.address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:street, :city, :state, :zip)
+  end 
+
 end
